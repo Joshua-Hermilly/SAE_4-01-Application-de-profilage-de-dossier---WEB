f17be313967762f1f8bdd98e58546195696e655e
HERMILLY Joshua <joshua.hermilly@etu.univ-lehavre.fr>
2026-03-26 15:42:07 +0100

    Ajout de l'entitée Filtre.php

---
+++ app/entities/Filtre.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace entities;
+
+class Filtre
+{
+	/*-------------------------------*/
+	/*  Construct                    */
+	/*-------------------------------*/
+	function __construct
+	(
+		private Groupe $groupe,
+		private array  $criteres = []
+	){}
+
+	/*-------------------------------*/
+	/*  Getters                      */
+	/*-------------------------------*/
+	public function getGroupe(): Groupe
+	{
+		return $this->groupe;
+	}
+
+	public function getCriteres(): array
+	{
+		return $this->criteres;
+	}
+
+	/*-------------------------------*/
+	/*  Setters                      */
+	/*-------------------------------*/
+	public function setGroupe(Groupe $groupe): void
+	{
+		$this->groupe = $groupe;
+	}
+
+	public function setCriteres(array $criteres): void
+	{
+		$this->criteres = $criteres;
+	}
+}
