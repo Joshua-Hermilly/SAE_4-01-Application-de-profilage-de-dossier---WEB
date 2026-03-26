690d803f29d00d6afd7f1fbd600bb71b8565e9f1
HERMILLY Joshua <joshua.hermilly@etu.univ-lehavre.fr>
2026-03-26 15:37:23 +0100

    Ajout de l'entitée Groupe.php

---
+++ app/entities/Groupe.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace entities;
+
+class Groupe
+{
+	/*-------------------------------*/
+	/*  Construct                    */
+	/*-------------------------------*/
+	function __construct
+	(
+		private int     $groupe_id,
+		private ?string $groupe_nom,
+		//private ?string $groupe_couleur,
+
+		private Filtre $filtre,
+		private array  $candidats = []
+	){}
+
+	/*-------------------------------*/
+	/*  Getters                      */
+	/*-------------------------------*/
+	public function getGroupeId(): int
+	{
+		return $this->groupe_id;
+	}
+
+	public function getGroupeNom(): ?string
+	{
+		return $this->groupe_nom;
+	}
+
+	public function getFiltre(): Filtre
+	{
+		return $this->filtre;
+	}
+
+	public function getCandidats(): array
+	{
+		return $this->candidats;
+	}
+
+	/*-------------------------------*/
+	/*  Setters                      */
+	/*-------------------------------*/
+	public function setGroupeId(int $groupe_id): void
+	{
+		$this->groupe_id = $groupe_id;
+	}
+
+	public function setGroupeNom(?string $groupe_nom): void
+	{
+		$this->groupe_nom = $groupe_nom;
+	}
+
+	public function setFiltre(Filtre $filtre): void
+	{
+		$this->filtre = $filtre;
+	}
+
+	public function setCandidats(array $candidats): void
+	{
+		$this->candidats = $candidats;
+	}
+}
