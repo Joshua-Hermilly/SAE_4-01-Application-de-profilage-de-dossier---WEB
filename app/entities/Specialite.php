86abbb66b5d1a415c0c22fc8749dc9256f0f48fb
Lucas Lafosse <apscawolf@gmail.com>
2026-03-26 15:34:31 +0100

    Refactor Diplome and Localisation entities to use constructor property promotion and add Specialite entity with getters and setters

---
+++ app/entities/Specialite.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace entities;
+
+class Specialite
+{
+	/*---------------------------------*/
+	/*          Constructeur           */
+	/*---------------------------------*/
+	function __construct(
+		private ?int $specialite_id,
+		private ?string $specialite_nom
+	) {}
+
+	/*---------------------------------*/
+	/*             Getters             */
+	/*---------------------------------*/
+	public function getSpecialiteId()
+	{
+		return $this->specialite_id;
+	}
+
+	public function getSpecialiteNom()
+	{
+		return $this->specialite_nom;
+	}
+
+	/*---------------------------------*/
+	/*             Setters             */
+	/*---------------------------------*/
+	public function setSpecialiteId($specialite_id): self
+	{
+		$this->specialite_id = $specialite_id;
+		return $this;
+	}
+
+	public function setSpecialiteNom($specialite_nom): self
+	{
+		$this->specialite_nom = $specialite_nom;
+		return $this;
+	}
+}
