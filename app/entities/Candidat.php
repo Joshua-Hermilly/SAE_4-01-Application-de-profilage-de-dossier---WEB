3b3a590420140567787001aee12d67fd97268216
HERMILLY Joshua <joshua.hermilly@etu.univ-lehavre.fr>
2026-03-26 14:58:28 +0100

    Ajout du début du entitie Candidat pour faire apparaitre la fetaure pour Lucas.

---
+++ app/entities/Candidat.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace entities;
+
+class Candidat
+{
+    private $candidat_code;
+    private $candidat_nom;
+    private $candidat_prenom;
+    private $candidat_civilite;
+    private $candidat_profil;
+    private $candidat_boursier_code;
+    private $candidat_note_lycee;
+    private $candidat_note_fiche;
+    private $candidat_note_global;
+    private $candidat_commentaire;
+}
