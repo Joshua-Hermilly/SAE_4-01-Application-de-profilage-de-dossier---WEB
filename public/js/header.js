dafb294bcd08c76f8f655216a62702a8287482eb
Prévost Donovan <donovan.prevost@etu.univ-lehavre.fr>
2026-03-31 10:53:21 +0200

    fix: ajout de la gestion du header pour changer de page

--- public/js/header.js
+++ public/js/header.js
@@ -2,9 +2,9 @@ const btnDossier    = document.getElementById("btnDossier"   );
 const btnGroupes    = document.getElementById("btnGroupes"   );
 const btnFormations = document.getElementById("btnFormations");
 
-btnDossier.addEventListener("click", () => changerPage("Dossiers.php") );
-btnDossier.addEventListener("click", () => changerPage("Groupes.php" ) );
-btnDossier.addEventListener("click", () => changerPage("Dossiers.php") );
+btnDossier.addEventListener   ("click", () => changerPage("index.php"     ) );
+btnGroupes.addEventListener   ("click", () => changerPage("groupes.php"   ) );
+btnFormations.addEventListener("click", () => changerPage("formations.php") );
 
 
 function changerPage(page)
