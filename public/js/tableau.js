d1ea746418f6e7addbb8ce1731372396ece4d9ae
HERMILLY Joshua <joshua.hermilly@etu.univ-lehavre.fr>
2026-04-01 19:14:43 +0200

    HOTFIX du ImportService.php avec la nouvelle base (déplacmeent de la relation Specialite Diplome en passant de 1..1 1..* à 1..* 1..1
    Diplome obtient la clé étrangé et specialite la perd.
    
    Modification du tableau pour ajouter les checkox, leurs checked est gérer dynamique et en session_storage

--- public/js/tableau.js
+++ public/js/tableau.js
@@ -2,6 +2,7 @@
 /* CONSTANTES             */
 /*------------------------*/
 // Tableau
+const tableau  = document.getElementById( "tableau"         );
 const dvErreur = document.getElementById( "erreur"          );
 const page     = document.getElementById( "page"            );
 const trHeader = document.getElementById( "trHead"          );
@@ -21,7 +22,7 @@ const infos    = document.getElementById( "pagination-info" );
 /*------------------------*/
 /* Fonctions              */
 /*------------------------*/
-function creerHeader( headers, dossiers )
+function creerHeader( headers, isAdmin )
 {
 	trHeader.innerHTML = "";
 
@@ -32,12 +33,29 @@ function creerHeader( headers, dossiers )
 		trHeader.innerHTML+= th;
 	}
 
-	creerTableau( headers, dossiers );
+	if (isAdmin)
+	{
+		const th    = document.createElement( 'th'    );
+		const input    = document.createElement( 'input' );
+		input.classList.add( "form-check-input" );
+		input.classList.add( "border-dark"      );
+		input.classList.add( "rounded-1"        );
+		input.classList.add( "cb"               );
+		input.id          = "cbTous";
+		input.type        = "checkbox";
+		th   .textContent = "Tout sélectionner";
+
+		if ( sessionStorage.getItem( input.id ) ) { input.checked = true; }
+
+		th      .appendChild( input );
+		trHeader.appendChild( th    );
+	}
 }
 
-function creerTableau( headers, dossiers )
+function creerTableau( headers, dossiers, isAdmin )
 {
-	tBody.innerHTML = "";
+	tBody.innerHTML  = "";
+
 	for ( let cptD = 0; cptD < dossiers.length; cptD++ )
 	{
 		const tr = document.createElement( 'tr' );
@@ -45,14 +63,22 @@ function creerTableau( headers, dossiers )
 
 		for ( let cptH = 0; cptH < headers.length-1; cptH++ )
 		{
-			const valeur = dossiers[cptD][ headers[cptH] ];
+			let valeur = dossiers[cptD][ headers[cptH] ];
+			if ( valeur ==    -1 ) { valeur = "NaN"         ;}
+			if ( valeur === null ) { valeur = "Non définie" ;}
+
 			let th;
 
 			if ( cptH === 0 )
 			{
 				th = `<th>
 					      <span class="badge border border-dark text-dark rounded-2 p-2">${valeur}</span>
-				      </th>`;
+				     </th>`;
+			}
+			else if ( valeur === "NaN" || valeur === "Non définie" )
+			{
+				th = `<th style="color: gray">${valeur}</th>`;
+
 			}
 			else
 			{
@@ -60,6 +86,25 @@ function creerTableau( headers, dossiers )
 			}
 
 			tr.innerHTML += th;
+		}
+
+		if (isAdmin)
+		{
+			const th    = document.createElement( 'th'    );
+			const input    = document.createElement( 'input' );
+			input.classList.add( "form-check-input" );
+			input.classList.add( "border-dark"      );
+			input.classList.add( "rounded-1"        );
+			input.classList.add( "cb"               );
+			input.id          =  "cb"+dossiers[cptD][headers[0]];
+			input.type        = "checkbox";
+
+			//console.log(input.id)
+			if ( sessionStorage.getItem( input.id         ) === "selectionner"                                        ) { input.checked = true; }
+			if ( sessionStorage.getItem( "cbTous"    ) && !sessionStorage.getItem( input.id ) !== "désélectionner") { input.checked = true; }
+
+			th.appendChild( input );
+			tr.appendChild( th    );
 		}
 
 		tBody.appendChild( tr );
@@ -73,19 +118,53 @@ function creerBtnPage( maxPage, actPage )
 	pageAct.textContent = actPage;
 	pageAct.value       = actPage;
 	btnFin.value        = maxPage;
-	btnSvt.disabled     = false;
-	btnPrc.disabled     = false;
-	btnFin.disabled     = false;
-	btnDeb.disabled     = false;
+	btnSvt.disabled     =  btnPrc.disabled = btnFin.disabled = btnDeb.disabled = false;
+	btnSvt.style.color = "#FFFFFFFF";
+	btnPrc.style.color = "#FFFFFFFF";
 
-	if ( maxPage === actPage ) {btnSvt.disabled = true;}
-	if ( actPage ===       1 ) {btnPrc.disabled = true;}
+	console.log(maxPage)
+	console.log(actPage+1)
+
+	if ( maxPage <= actPage+1  ) {btnSvt.disabled = true; btnSvt.style.color =  "#3f3f3f;"; }
+	if ( actPage ==         1  ) {btnPrc.disabled = true; btnPrc.style.color =  "#3d3b3b;"; }
 }
 
 function afficherErreur( erreur )
 {
 	dvErreur.textContent   = erreur;
 	dvErreur.style.display = "block";
+}
+
+function selectionFaite(event)
+{
+	if ( event.target.tagName === 'INPUT')
+	{
+		if ( event.target.id === "cbTous" )
+		{
+			const lstCb = document.getElementsByClassName( "cb" );
+			if ( event.target.checked )
+			{
+				sessionStorage.clear();
+				sessionStorage.setItem( event.target.id, 'selectionner' );
+				for ( let cpt = 0; cpt < lstCb.length; cpt++ ) { lstCb[cpt].checked = true; }
+			}
+			else
+			{
+				sessionStorage.removeItem( event.target.id );
+				for ( let cpt = 0; cpt < lstCb.length; cpt++ ) { lstCb[cpt].checked = false; }
+			}
+		}
+		else if ( sessionStorage.getItem( "cbTous" ) )
+		{
+			if ( event.target.checked ) { sessionStorage.setItem   ( event.target.id, 'désélectionner' ); }
+			else                        { sessionStorage.removeItem( event.target.id                   ); }
+		}
+		else
+		{
+			if ( event.target.checked ) { sessionStorage.setItem   ( event.target.id, 'selectionner'); event.target.checked = true ; }
+			else                        { sessionStorage.removeItem( event.target.id                ); event.target.checked = false; }
+		}
+	}
 }
 
 /*------------------------*/
@@ -106,7 +185,8 @@ async function getDossierCandidat( indexPage )
 		});
 
 		const donnees = await response.json();
-		console.log(donnees)
+		//console.log(donnees       )
+		//console.log(sessionStorage)
 
 		if (!response.ok) { throw new Error(`Erreur ${response.status}: ${donnees}`); }
 
@@ -118,14 +198,15 @@ async function getDossierCandidat( indexPage )
 
 		if ( donnees['dossiers'] !== null )
 		{
-			document.getElementById("tableau").style.display = "";
+			tableau.style.display = "";
 		}
 		else
 		{
 			document.getElementById( "vide"    ).style.display = "block";
 		}
-		creerHeader ( donnees['headers' ], donnees['dossiers'] );
-		creerBtnPage( donnees['maxPage' ], donnees['actPage' ] );
+		creerHeader ( donnees['headers' ], donnees['isAdmin' ]                     );
+		creerTableau( donnees['headers' ], donnees['dossiers'], donnees['isAdmin'] );
+		creerBtnPage( donnees['maxPage' ], donnees['actPage' ]                     );
 
 	} catch (error) { console.error('Erreur :', error); }
 }
@@ -136,7 +217,8 @@ getDossierCandidat(1);
 /* Event                  */
 /*------------------------*/
 // tBody.addEventListener ( "click", () => getDossierCandidat(             1) );
-btnPrc.addEventListener( "click", () => getDossierCandidat(+pageAct.value - 1) );
-btnSvt.addEventListener( "click", () => getDossierCandidat(+pageAct.value + 1) );
-btnDeb.addEventListener( "click", () => getDossierCandidat(   1) );
-btnFin.addEventListener( "click", () => getDossierCandidat(   btnFin.value) );
+tableau.addEventListener( "click",(event) => selectionFaite    (event)  );
+btnPrc.addEventListener( "click", ()    => getDossierCandidat(+pageAct.value - 1) );
+btnSvt.addEventListener( "click", ()    => getDossierCandidat(+pageAct.value + 1) );
+btnDeb.addEventListener( "click", ()    => getDossierCandidat(   1) );
+btnFin.addEventListener( "click", ()    => getDossierCandidat(   btnFin.value) );
