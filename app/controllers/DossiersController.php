8772213690f85a0aa0c30c2b261777351ca4273f
Lucas Lafosse <lucas.lafosse@etu.univ-lehavre.fr>
2026-04-02 17:27:13 +0200

    refactor: simplify filter configuration logic in DossiersController

--- app/controllers/DossiersController.php
+++ app/controllers/DossiersController.php
@@ -11,18 +11,7 @@ class DossiersController extends Controller
 		if (session_status() === PHP_SESSION_NONE) { session_start(); }
 
 		$filtreService = new DossierFiltreService();
-		$dataVersion   = $_SESSION['data_version'] ?? '0';
-		$filtreCache   = $_SESSION['filter_cache'] ?? null;
-
-		if (is_array($filtreCache) && ($filtreCache['version'] ?? '') === $dataVersion) { $filterConfig = $filtreCache['data']; }
-		else
-		{
-			$filterConfig = $filtreService->buildFilterConfig();
-			$_SESSION['filter_cache'] = [
-				'version' => $dataVersion,
-				'data'    => $filterConfig,
-			];
-		}
+		$filterConfig  = $filtreService->buildFilterConfig();
 
 		$this->view('pages/dossiers', 'Dossiers', ['filterConfig' => $filterConfig]);
 	}
