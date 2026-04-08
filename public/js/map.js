d151703bfc3975a524f9f127e2996734d0aa72f4
HERMILLY Joshua <joshua.hermilly@etu.univ-lehavre.fr>
2026-04-08 22:24:47 +0200

    Cache log

--- public/js/map.js
+++ public/js/map.js
@@ -68,7 +68,7 @@ async function getCarte(distance)
         const donnees = await response.json();
         if (donnees.erreur) { console.error('Erreur API:', donnees.erreur); return; }
 
-        console.log(donnees);
+        //console.log(donnees);
 
         initSide(donnees.max_distance);
 
